0b8ced3accf485c26cead8a0bb7aad5f7d41322c
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-11-04 20:34:22 +0300

    add pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 import {setupMobileMenu} from './mobileMenu.js';
 
 setupMobileMenu();
+
+document.querySelectorAll(".desktop").forEach(link => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const targetId = link.getAttribute("href").slice(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY - 40;
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+      });
+    }
+  });
+});
