149113baac370e1c5fdcb35b3acc7be177b8ef5e
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-11-05 20:39:20 +0300

    header hotfix

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,39 @@ import {setupMobileMenu} from './mobileMenu.js';
 
 setupMobileMenu();
 
+document.querySelectorAll(".desktop").forEach(link => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    link.blur();
+
+    const header = document.querySelector(".header");
+    header.classList.add('no-hover');
+
+    const targetId = link.getAttribute("href").slice(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      const currentScrollPos = window.scrollY;
+      const headerHeight = header ? header.offsetHeight : 0;
+      const offsetTop = targetSection.getBoundingClientRect().top + currentScrollPos - headerHeight;
+
+      if (offsetTop < currentScrollPos && header) {
+        header.classList.remove('scroll');
+      }
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+      });
+    }
+
+    setTimeout(() => {
+      header.classList.remove('no-hover');
+    }, 500);
+  });
+});
+
 let prevScrollpos = window.scrollY;
 let timeout;
 const header = document.getElementById("header");
@@ -24,28 +57,3 @@ window.onscroll = function () {
 
   prevScrollpos = currentScrollPos;
 };
-
-document.querySelectorAll(".desktop").forEach(link => {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    const targetId = link.getAttribute("href").slice(1);
-    const targetSection = document.getElementById(targetId);
-    const header = document.querySelector(".header");
-
-    if (targetSection) {
-      const currentScrollPos = window.scrollY;
-      const headerHeight = header ? header.offsetHeight : 0;
-      const offsetTop = targetSection.getBoundingClientRect().top + currentScrollPos - headerHeight;
-
-      if (offsetTop < currentScrollPos && header) {
-        header.classList.remove('scroll');
-      }
-
-      window.scrollTo({
-        top: offsetTop,
-        behavior: "smooth"
-      });
-    }
-  });
-});
